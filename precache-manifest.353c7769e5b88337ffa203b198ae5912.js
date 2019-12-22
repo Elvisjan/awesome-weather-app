392ef044d025510f8ce8356ed4e1bd417d7dd412
Ilgam Tukhvatullin <ilgamtukhvatullin91@gmail.com>
2019-12-22 19:44:25 +0500

    Updates

---
+++ precache-manifest.353c7769e5b88337ffa203b198ae5912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9051c1db78e7d5032943f355384b057",
+    "url": "/awesome-weather-app/index.html"
+  },
+  {
+    "revision": "96c7dfa0d812d32bce23",
+    "url": "/awesome-weather-app/static/js/2.2d85a310.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/awesome-weather-app/static/js/2.2d85a310.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d3a052d67326ca85cbb",
+    "url": "/awesome-weather-app/static/js/main.09ef2f1e.chunk.js"
+  },
+  {
+    "revision": "249a4e561db30a09ce0d",
+    "url": "/awesome-weather-app/static/js/runtime-main.08faae54.js"
+  }
+]);
