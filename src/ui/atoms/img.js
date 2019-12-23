391e63a085576ec29473e9199d85efde2f169275
Ilgam Tukhvatullin <ilgamtukhvatullin91@gmail.com>
2019-12-23 23:19:21 +0500

    Added gap

--- src/ui/atoms/img.js
+++ src/ui/atoms/img.js
@@ -3,5 +3,4 @@ import styled from 'styled-components'
 export const Img = styled.img`
   width: 30px;
   height: 30px;
-  margin-left: -10px;
 `
--- src/ui/atoms/listItem.js
+++ src/ui/atoms/listItem.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const ListItem = styled.li`
   display: grid;
   margin-left: -15px;
+  grid-gap: 20px;
   grid-template-columns: 1fr 20px 120px;
   color: #fff;
   font-size: 17px;
