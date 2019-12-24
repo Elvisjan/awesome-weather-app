bb726a4a4f6536c58c7f8b04a347ae1ef686905d
Ilgam Tukhvatullin <ilgamtukhvatullin91@gmail.com>
2019-12-24 22:44:59 +0500

    Some fixes

--- src/pages/home/model.js
+++ src/pages/home/model.js
@@ -1,46 +1,53 @@
-import { sample } from 'effector'
-import { createStore, createEvent } from '../../features/common'
-import { deleteItem, fetching, triggerSample } from '../lib'
+import { sample } from "effector"
+import { createStore, createEvent } from "../../features/common"
+import { deleteItem, fetching, triggerSample } from "../lib"
 
-export const addCity = createEvent('add city')
-export const addValue = createEvent('add input value')
+export const addCity = createEvent("add city")
+export const addValue = createEvent("add input value")
 
-export const $cityNames = createStore(["Москва"], { name: 'cityNames' })
+export const $cityNames = createStore(["Москва"], { name: "cityNames" })
   .on(fetching.done, (state, { result, params }) => {
     if (result.cod === 200) {
       state = [...state]
-      const oldValue = state.find(item => item.toLowerCase() === params.toLowerCase())
+      const oldValue = state.find(
+        (item) => item.toLowerCase() === params.toLowerCase()
+      )
       if (oldValue) return
       else {
         state.push(params)
       }
       return state
     } else return
-  }
-  )
+  })
   .on(deleteItem, (state, index) => state.filter((_, i) => i !== index))
 
-export const $cityList = createStore(
-  [
-  ], { name: 'cityList' })
+export const $cityList = createStore([], { name: "cityList" })
   .on(deleteItem, (state, index) => state.filter((_, i) => i !== index))
   .on(fetching.done, (state, { result, params }) => {
     state = [...state]
-    const oldValue = state.find(item => item.name.toLowerCase() === params.toLowerCase())
+    const oldValue = state.find(
+      (item) => item.name.toLowerCase() === params.toLowerCase()
+    )
     if (oldValue) {
       state[state.indexOf(oldValue)] = {
-        ...oldValue, temperature: result.main.temp.toFixed(), feels: result.main.feels_like.toFixed(),
-        weather: result.weather
+        ...oldValue,
+        temperature: result.main.temp.toFixed(),
+        feels: result.main.feels_like.toFixed(),
+        weather: result.weather,
       }
     } else {
       state.push({
-        name: params.toUpperCase(), weather: result.weather, temperature: result.main.temp, cod: result.cod, id: result.id,
-        feels: result.main.feels_like
+        name: params.toUpperCase(),
+        weather: result.weather,
+        temperature: result.main.temp,
+        cod: result.cod,
+        id: result.id,
+        feels: result.main.feels_like,
       })
     }
     return state
   })
-$cityList.getState().length < 1 && fetching('МОСКВА')
+$cityList.getState().length < 1 && fetching("МОСКВА")
 
 export const $inputValue = createStore("")
   .on(addValue, (_, e) => e.currentTarget.value)
@@ -49,5 +56,5 @@ export const $inputValue = createStore("")
 sample({
   source: $inputValue,
   clock: triggerSample,
-  target: fetching
+  target: fetching,
 })
--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { useStore, useList, createComponent } from 'effector-react';
+import React from "react"
+import { useStore, useList, createComponent } from "effector-react"
+import { $cityList, $cityNames, $inputValue, addValue } from "./model"
+import { inputHandler, fetching, deleteItem, refetchWeather } from "../lib"
 import {
-  $cityList, $cityNames, $inputValue, addValue
-} from './model'
-import { inputHandler, fetching, deleteItem, refetchWeather } from '../lib'
-import { Header, H1, Text, Spinner, Section, List, ListItem, Button, Img, Search } from '../../ui';
+  Header,
+  H1,
+  Text,
+  Spinner,
+  Section,
+  List,
+  ListItem,
+  Button,
+  Img,
+  Search,
+} from "../../ui"
 
 export const HomePage = () => {
   const names = useStore($cityNames)
   React.useEffect(() => {
     const timer = setInterval(() => {
       refetchWeather(names)
-    }, 60000);
-    return () => clearInterval(timer);
-  }, [names]);
+    }, 60000)
+    return () => clearInterval(timer)
+  }, [names])
 
   return (
     <>
@@ -22,27 +31,47 @@ export const HomePage = () => {
         <SpinnerContainer spinner={Spinner}></SpinnerContainer>
       </Header>
       <Section>
-        <Search value={useStore($inputValue)}
-          changer={addValue} type='text'
-          placeholder='Например: Нижний Новгород'
-          submiter={inputHandler} label='Введите название города' />
-        {useStore($cityList).length > 0 ? <CityList /> : <Text>Добавьте город</Text>}
+        <Search
+          value={useStore($inputValue)}
+          changer={addValue}
+          type="text"
+          placeholder="Например: Нижний Новгород"
+          submiter={inputHandler}
+          label="Введите название города"
+        />
+        {useStore($cityList).length > 0 ? (
+          <CityList />
+        ) : (
+          <Text>Добавьте город</Text>
+        )}
       </Section>
     </>
-  );
+  )
 }
-const CityList = () => useList($cityList, ({ name, weather, temperature, cod, feels }, index) => {
-  const listItem =
-    cod === 200 ? <ListItem>
-      В городе {name} в данный момент  {weather[0].description}, температура воздуха
-      составляет {parseInt(temperature)} &#176;
-      по цельсию, чувствуется как {parseInt(feels)}&#176;
-      по цельсию <Img alt='погодное условие' src={`http://openweathermap.org/img/w/${weather[0].icon}.png`} /><Button type='button' onClick={() => {
-        deleteItem(index)
-      }}>Удалить</Button>
-    </ListItem> : null
-  return <List>{listItem}</List>
-})
-const SpinnerContainer = createComponent(fetching.pending, (_, pending) => (
+const CityList = () =>
+  useList($cityList, ({ name, weather, temperature, cod, feels }, index) => {
+    const listItem =
+      cod === 200 ? (
+        <ListItem>
+          В городе {name} в данный момент {weather[0].description}, температура
+          воздуха составляет {parseInt(temperature)} &#176; по цельсию,
+          чувствуется как {parseInt(feels)}&#176; по цельсию{" "}
+          <Img
+            alt="погодное условие"
+            src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
+          />
+          <Button
+            type="button"
+            onClick={() => {
+              deleteItem(index)
+            }}
+          >
+            Удалить
+          </Button>
+        </ListItem>
+      ) : null
+    return <List>{listItem}</List>
+  })
+const SpinnerContainer = createComponent(fetching.pending, (_, pending) =>
   pending ? <Spinner>Идёт загрузка, пожалуйста, подождите</Spinner> : null
-))
+)
