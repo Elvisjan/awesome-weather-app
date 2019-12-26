de8e36a38596acdca9c3e478e7e430bba4757407
Ilgam Tukhvatullin <ilgamtukhvatullin91@gmail.com>
2019-12-26 11:11:13 +0500

    Changing overflow

--- src/global-styles.js
+++ src/global-styles.js
@@ -3,8 +3,6 @@ import { createGlobalStyle } from "styled-components"
 export const GlobalStyles = createGlobalStyle`
 body {
   height: 100vh;
-  overflow-x: hidden;
-  overflow-y: scroll;
   display: grid;
 }
 `
